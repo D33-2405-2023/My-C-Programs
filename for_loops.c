7df4080154df3ad1b866750f5ed60599187af771
D33-2405-2023 <nderebamaureen2022@gmail.com>
2025-03-08 16:56:49 +0300

    Add files via upload

---
+++ for_loops.c
@@ -0,0 +1,18 @@
+/*
+for loops
+Autho: Maureen Ndereba 
+adm:D33-2405-2023
+date:8-03-2025
+*/
+#include <stdio.h>
+
+int main() {
+   int i=1;
+   //for(start; stop; step)
+   for(i=1; i<=100; i=i+1){
+       
+       
+        printf("number is :%d, and  cube of the %d is:%d\n",i,i, (i*i*i)) ; 
+  }
+    return 0;
+}
